ed6e23cbe3e8bbe3cc7f40f0ec1465942c45dc03
kushal-sinha <kushal147258369@gmail.com>
2024-04-25 19:53:38 +0530

    added image component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image";
 
-export default function loading() {
+export default function Loading() {
     return (
-        <div className='flex justify-center'>
-            <img className=' h-52' src='spinner.svg' alt='loading' />
+        <div className='flex justify-center mt-16'>
+            <Image
+                className='h-52'
+                src='/spinner.svg'
+                alt='Loading...'
+                width={100}
+                height={100} // Adjust the width and height as needed
+            />
         </div>
-    )
+    );
 }
